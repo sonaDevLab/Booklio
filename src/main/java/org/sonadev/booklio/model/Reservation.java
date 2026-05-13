9e47df7967a7ffb3da11777f6ca32d8581cbba22
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-05-13 22:16:24 +0200

    Models created

---
+++ src/main/java/org/sonadev/booklio/model/Reservation.java
@@ -0,0 +1,33 @@
+package org.sonadev.booklio.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "resource_id")
+    private Resource resource;
+
+}
