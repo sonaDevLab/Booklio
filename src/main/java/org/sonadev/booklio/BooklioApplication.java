ef69c81d5937aa0238503184018185662c3bab3f
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-05-13 22:13:40 +0200

    Initial commit

---
+++ src/main/java/org/sonadev/booklio/BooklioApplication.java
@@ -0,0 +1,13 @@
+package org.sonadev.booklio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sonadev/booklio/BooklioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sonadev.booklio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
