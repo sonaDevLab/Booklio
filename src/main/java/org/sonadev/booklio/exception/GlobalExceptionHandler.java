92842e0ed838b4fbd1e4a19d26024ca6dd08ca4d
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-05-16 14:55:37 +0200

    Global handler modified to implement custom exceptions correctly with its own status codes

--- src/main/java/org/sonadev/booklio/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sonadev/booklio/exception/GlobalExceptionHandler.java
@@ -1,26 +1,50 @@
 package org.sonadev.booklio.exception;
 
+import org.sonadev.booklio.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex) {
 
-        ex.getBindingResult().getFieldErrors()
-                .forEach(error ->
-                        errors.put(error.getField(), error.getDefaultMessage())
-                );
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                404,
+                ex.getMessage()
+        );
 
-        return ResponseEntity.badRequest().body(errors);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(ReservationConflictException.class)
+    public ResponseEntity<ErrorResponse> handleConflict(ReservationConflictException ex) {
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                409,
+                ex.getMessage()
+        );
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(InvalidReservationException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidReservation(InvalidReservationException ex) {
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                400,
+                ex.getMessage()
+        );
+
+        return ResponseEntity.badRequest().body(error);
     }
 
 }
