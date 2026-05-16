4e74c452e550610315928cd7840f89d74c32279c
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-05-16 14:50:00 +0200

    Error response DTO created

---
+++ src/main/java/org/sonadev/booklio/dto/ErrorResponse.java
@@ -0,0 +1,16 @@
+package org.sonadev.booklio.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+
+}
