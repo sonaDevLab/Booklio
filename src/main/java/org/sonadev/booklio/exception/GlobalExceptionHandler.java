61878a106c55364dd8da69419e64ae5ac017ed2f
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-05-16 14:20:52 +0200

    Global handler update to return errors with its corresponding message

--- src/main/java/org/sonadev/booklio/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sonadev/booklio/exception/GlobalExceptionHandler.java
@@ -1,15 +1,26 @@
 package org.sonadev.booklio.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handle(RuntimeException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error ->
+                        errors.put(error.getField(), error.getDefaultMessage())
+                );
+
+        return ResponseEntity.badRequest().body(errors);
     }
 
 }
