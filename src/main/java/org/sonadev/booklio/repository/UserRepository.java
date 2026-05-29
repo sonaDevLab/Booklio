7e59b3ed31e603c5efa266ff8a47091ec07e1ca2
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-05-29 13:24:58 +0200

    Added find by email method to User repository

--- src/main/java/org/sonadev/booklio/repository/UserRepository.java
+++ src/main/java/org/sonadev/booklio/repository/UserRepository.java
@@ -3,5 +3,12 @@ package org.sonadev.booklio.repository;
 import org.sonadev.booklio.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
 }
