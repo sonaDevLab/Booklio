eef67646f827ef608d506135384233b884aec687
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-06-04 13:48:43 +0200

    Response for Auth dto created for Login auth

---
+++ src/main/java/org/sonadev/booklio/dto/AuthResponse.java
@@ -0,0 +1,12 @@
+package org.sonadev.booklio.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+
+}
