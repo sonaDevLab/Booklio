ced96c9269b02fcdbec3edbb02acf738b6d808f4
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-06-09 14:11:48 +0200

    UserDetailsService created to return User details by email

---
+++ src/main/java/org/sonadev/booklio/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package org.sonadev.booklio.service;
+
+import lombok.AllArgsConstructor;
+import org.sonadev.booklio.model.User;
+import org.sonadev.booklio.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+
+}
