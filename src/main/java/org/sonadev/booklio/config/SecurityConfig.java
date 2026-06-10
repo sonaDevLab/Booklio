0621b244fb0f4bf816e082d807609d5e0e731b99
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-06-10 21:19:25 +0200

    CORS added to security filter chain

--- src/main/java/org/sonadev/booklio/config/SecurityConfig.java
+++ src/main/java/org/sonadev/booklio/config/SecurityConfig.java
@@ -61,6 +61,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(
+                        corsConfigurationSource()
+                ))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
